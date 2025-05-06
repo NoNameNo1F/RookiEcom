cf6a2871521f61c373fc301b5013bc804cb4fc17
Vu Nguyen Cao Nam <nonameno1f@gmail.com>
2025-05-06 23:22:15 +0700

    test(product-rating): test for create rating command and get all rating of an product

---
+++ src/Modules/ProductModule/RookiEcom.Modules.Product.Application.UnitTests/ProductRatings/ProductRatingQueryServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using RookiEcom.Modules.Product.Application.Queries;
+using RookiEcom.Modules.Product.Application.UnitTests.Abstractions;
+
+namespace RookiEcom.Modules.Product.Application.UnitTests.ProductRatings;
+
+public class ProductRatingQueryServiceTests : BaseServiceTest
+{
+    [Theory]
+    [InlineData(1,1,3,3)]
+    [InlineData(2, 2,4,2)]
+    public async Task GetProductRatings_ValidPageAndProductId_ReturnsPagedResult(
+        int productId, 
+        int pageNumber, 
+        int pageSize, 
+        int expectedTotalCount)
+    {
+        // Arrange
+        var (scope, productRatingService) = GetScopedService<ProductRatingService>();
+        
+        // Act
+        var ratingResult = await productRatingService
+            .GetRatingsPaging(productId,pageNumber, pageSize,default);
+        
+        // Assert
+        Assert.Equal(expectedTotalCount, ratingResult.PageData.TotalCount);
+    }
+
+    [Theory]
+    [InlineData(99, 1, 3, 0)]
+    [InlineData(87, 1, 3, 0)]
+    public async Task GetProductRatings_ProductIdNotFound_ReturnsEmptyPagedResult(
+        int productId,
+        int pageNumber,
+        int pageSize,
+        int expectedTotalCount)
+    {
+        // Arrange
+        var (scope, productRatingService) = GetScopedService<ProductRatingService>();
+        
+        // Act
+        var ratingResult = await productRatingService
+            .GetRatingsPaging(productId, pageNumber, pageSize, default);
+        
+        // Assert
+        Assert.Equal(expectedTotalCount, ratingResult.PageData.TotalCount);
+    }
+}
